b2cb8f69a91c72030f5636ce35805efff166938f
Ksulu <kn1159552776@163.com>
2022-08-16 17:25:37 +0800

    fist commit

---
+++ src/main/java/com/kn/Git/test.java
@@ -0,0 +1,14 @@
+package com.kn.Git;
+
+public class test {
+
+    public void Test(){
+        System.out.println("hello,Git!!");
+        System.out.println("111");
+        int i = 1;
+        System.out.println("12345");
+        System.out.println("234556");
+    }
+
+
+}
