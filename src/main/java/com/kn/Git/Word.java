4760be762be0dfa35840cf869a211c50b3f5e480
Ksulu <kn1159552776@163.com>
2022-08-16 22:39:03 +0800

    word3 commit

--- src/main/java/com/kn/Git/Word.java
+++ src/main/java/com/kn/Git/Word.java
@@ -5,5 +5,6 @@ public class Word {
         System.out.println("Word master");
         System.out.println("word 修改");
         System.out.println("word2 修改");
+        System.out.println("push ");
     }
 }
